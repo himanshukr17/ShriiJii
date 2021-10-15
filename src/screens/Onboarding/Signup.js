05d5f0c7636313f2c3edb1668b76a2f22b25b389
unknown <ayushtomar692@gmail.com>
2021-10-16 00:49:39 +0530

    Signup screen

--- src/screens/Onboarding/Signup.js
+++ src/screens/Onboarding/Signup.js
@@ -1,6 +1,6 @@
 import React, {memo} from 'react';
-import {ThemeProvider} from 'react-native-elements';
-
+import {ThemeProvider,Icon} from 'react-native-elements';
+import {TouchableOpacity} from 'react-native';
 import Background from '../../components/Background';
 import Container from '../../components/Layout/container';
 import Header from '../../components/Layout/header';
@@ -10,8 +10,11 @@ import {Text, View, StyleSheet,Image,KeyboardAvoidingView} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Typography from '../../components/Typography/Typography';
 import TextInput from '../../components/CustomInputs/PasswordField';
-import IconButton from '../../components/CustomButtons/IconButton';
+// import Icon from '../../components/CustomButtons/IconButton';
+import { Divider } from 'react-native-paper';
 import Nav from "../../components/Headers/header"
+import Button from '../../components/CustomButtons/Button';
+import InputField from "../../components/CustomInputs/InputField";
 import PhoneNumberInput from '../../components/CustomInputs/PhoneInput';
 const SignupScreen = ({navigation}) => (
   <Container>
@@ -22,61 +25,104 @@ const SignupScreen = ({navigation}) => (
         }
         rightComponent={
           <View style={{ marginRight: 10 }}>
-            <Typography color={"textSecondary"} size={15} type={"light"}>SKIP</Typography>
+            <Typography color={"textTertiary"} size={15} type={"light"}>SKIP</Typography>
           </View>
         }
       />
     </Header>
+
     <Content>
 
-   <View style={styles.main}>
-
    <View style={styles.inputView}>
-
-        <Typography size={22} style={{backgroundColor:"red"}} type={'medium'} style={styles.title}>
-          Enter your mobile number
-        </Typography>
-       <View style={{marginTop:20}}>
-        <PhoneNumberInput />
-
+   <View style={{flexDirection:'row',justifyContent:'center',marginBottom:10}}>
+   <TouchableOpacity>
+    <Icon
+          name={'logo-google'}
+          type={'ionicon'}
+          style={{borderRadius:'50%'}}
+          reverse={true}
+          color={'#bb1409'}
+        />
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Icon
+          name={'facebook'}
+          type={'feather'}
+          style={{borderRadius:'50%'}}
+          reverse={true}
+          color={'#3b5998'}
+        />
+        </TouchableOpacity>
+       </View>
+       <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '20%',marginTop:15 }}>
+          <Divider style={{ backgroundColor: "grey", height: 0.5, width: '45%', marginLeft: -3 }} />
+          <Typography size={15} type={'bold'}> OR </Typography>
+          <Divider style={{ backgroundColor: "grey", height: 0.5, width: '45%', }} />
         </View>
 
-      
+        <InputField placeholder="Email Address" />
+        <View style={{ marginTop: -4 }}>
+          <TextInput placeholder="Password" />
+         {/* <View style={styles.forgetPass}> 
+            <Typography size={14} type={"semiBold"} color={"yellow"} >Forget Password?</Typography>
+          </View> */}
         </View>
-
-        <View style={styles.bottomButton}>
-        <View style={{width:58}}>
-        <IconButton padding={10} iconName={"chevron-right"} iconSize={35} onPress={()=>navigation.navigate("Otp")}  borderRadius={30}/>
-        </View>
-
-        </View>
-        </View>
+        
+      </View>
     </Content>
-    <Footer></Footer>
+    <Footer>
+    <View style={{ alignItems: 'center', marginTop: 10 }}>
+        <Button style={{ marginTop: 20, width: '40%' }} title="Sign Up" />
+        <View style={{ flexDirection: 'row', marginTop: 10, marginBottom: 10 }}>
+          <Typography color="textSecondary">ALREADY HAVE AN ACCOUNT? </Typography>
+          <Typography color="textPrimary" type={"extraBold"}>SIGN IN.</Typography>
+        </View>
+      </View>
+
+    </Footer>
   </Container>
 );
 
 const styles = StyleSheet.create({
   inputView: {
     flex: 1,
+    marginTop:50,
     justifyContent: 'flex-start',
   },
   title: {
     marginLeft: 10,
 
   },
-  main:{
-      flex:1,
-      justifyContent: 'space-between'
+
+  subtitle: {
+    marginBottom: 30,
+    marginLeft: 10,
+    marginTop: 8
   },
-  bottomButton:{
-      width:"100%",
-  paddingBottom:20,
-      flexDirection:"column",
-      justifyContent:"flex-end",
-      padding:10,
-      alignItems:"flex-end"
+  forgetPass: {
+
+    flexDirection: 'row',
+    justifyContent: "flex-end"
+
+  },
+  tinyLogo: {
+    height: 50,
+    marginTop: 40,
+    resizeMode: "contain"
+  },
+  image: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 30
+
+  },
+  signin: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 20
   }
+
 
 });
 
