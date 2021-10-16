838e2d07b563c41aa0cf50bc2abd4aa26de375ce
unknown <ms5247556@gmail.com>
2021-10-16 20:53:41 +0530

    last screen

--- src/screens/Claims/Order.js
+++ src/screens/Claims/Order.js
@@ -1,6 +1,8 @@
-import React, { memo } from 'react';
-import { ThemeProvider } from 'react-native-elements';
-
+import React, { memo,useState } from 'react';
+import { ThemeProvider,ListItem } from 'react-native-elements';
+import {Rating, AirbnbRating} from 'react-native-ratings';
+import DropDownPicker from 'react-native-dropdown-picker';
+import NumericInput from 'react-native-numeric-input'
 import Background from '../../components/Background';
 import Container from '../../components/Layout/container';
 import Header from '../../components/Layout/header';
@@ -13,19 +15,31 @@ import TextInput from '../../components/CustomInputs/PasswordField';
 import BottomNav from "../../components/BottomNav/Main"
 import IconButton from '../../components/CustomButtons/IconButton';
 import { Icon, Avatar } from 'react-native-elements';
+import { Divider } from 'react-native-paper';
 import { theme } from "../../config/theme"
 import Nav from "../../components/Headers/header"
 import Banner from "../../components/Misc/BannerCard"
 import Search from '../../components/CustomInputs/SearchField';
 import AvatarList from '../../components/List/AvatarList';
-import { Divider } from 'react-native-paper'
+// import { Divider } from 'react-native-paper'
 import Button from '../../components/CustomButtons/Button';
 import { BlurView } from "@react-native-community/blur";
 import PhoneNumberInput from '../../components/CustomInputs/PhoneInput';
 import { BoxShadow } from 'react-native-shadow'
 import Card from "../../components/Cards/ProductCardH"
 import { TouchableOpacity } from 'react-native-gesture-handler';
-const LoginScreen = ({ navigation }) => (
+const LoginScreen = ({ navigation }) => {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        {label: 'Red', value: '0'},
+        {label: 'Blue', value: '1'},
+        {label: 'Black', value: '2'},
+        {label: 'White', value: '3'}
+      ]);
+
+    return(
+        
     <Container>
         <Header  >
             <Nav
@@ -78,19 +92,88 @@ const LoginScreen = ({ navigation }) => (
             </View> */}
             <View >
                 <Avatar border={6} source={{ uri: 'https://cdn.pixabay.com/photo/2016/08/12/14/25/abstract-1588720_960_720.jpg' }} style={{ resizeMode: 'contain', height: 250, width: '100%' }}>
-                    <Avatar.Accessory type="ionicon" name="heart" color="#a8a8a8" style={{ backgroundColor: 'white', marginTop: -100, top: 105, left:5 }} size={35} />
+                    
                     <Avatar.Accessory type="ionicon" name="heart" color="#a8a8a8" style={{ backgroundColor: 'white', marginTop: -100, top: 105, right: 5 }} size={35} />
                     <Avatar.Accessory type="ionicon" name="share-social" color="#a8a8a8" style={{ backgroundColor: 'white', marginTop: -100, bottom: 5, right: 5 }} size={35} />
+                    
                 </Avatar>
             </View>
-
-
+             <ListItem>
+                 <ListItem.Content marginLeft={-10}>
+            <ListItem.Title size={14} numberOfLines={2}  type={"extraBold"}>PRODUCT NAME NAME OF THE PRODUCT (WITH FULL DESCRIPTION IN TWO LINES AND THEN ELIPSIS)</ListItem.Title>
+<ListItem.Subtitle>Category Name</ListItem.Subtitle>
+</ListItem.Content>
+</ListItem>
+{/* <AirbnbRating
+  count={11}
+//   reviews={["Terrible", "Bad", "Meh", "OK", "Good", "Hmm...", "Very Good", "Wow", "Amazing", "Unbelievable", "Jesus"]}
+  defaultRating={11}
+  size={20}
+/> */}
+<Rating
+  type='star'
+  ratingCount={5}
+  showRating={false}
+  imageSize={24}
+  style={{alignItems:'flex-start',marginLeft:10}}
+//   showRating
+//   onFinishRating={this.ratingCompleted}
+/>
+<View style={{flexDirection : 'row',alignItems:'center',alignContent:'center'}}>
+<Typography size={25} style={{marginTop:10,marginLeft:10}}>Rs.13000</Typography>
+<ListItem.Subtitle style={styles.subtitleStyle2}>Rs.15000/-</ListItem.Subtitle>
+</View>
+< View style = {{marginTop:10, marginLeft : 10, marginBottom: 20,flexDirection:'row',justifyContent:'space-between'}}>
+<View style={{alignItems:'center'}}>
+    <Typography size={15} style={{marginBottom:2}}> Quantity : </Typography>
+<NumericInput 
+            // value={this.state.value} 
+            // onChange={value => this.setState({value})} 
+            // onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+            totalWidth={130} 
+            totalHeight={35} 
+            iconSize={25}
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#000000' 
+            iconStyle={{ color: 'white' }} 
+            minValue = {0}
+            rightButtonBackgroundColor='#c0c0c0' 
+            leftButtonBackgroundColor='#c0c0c0'/>
+            </View>
+            <View style={{marginRight:10,marginTop:0, alignItems:'center'}}>
+                <Typography size={15} style={{marginBottom:2}}>Colour: </Typography>
+            <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      style={{width:80,height:35}}
+    />
+    </View>
+</View>
+            {/* <Typography size={12}  type={"semiBold"} >Category Name</Typography> */}
+            <View style={{marginLeft:10,marginRight:10}}>
+            <Typography size={14}  type={"extraBold"}>PRODUCT DESCRIPTION</Typography>
+            <Typography size={13} style={{marginTop:1}}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr</Typography>
+            <Typography size={14} style={{marginTop:30}} type={"extraBold"}>STORY BEHIND THE CATEGORY</Typography>
+            <Typography size={13} style={{marginTop:1}}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren</Typography>
+            </View>
         </Content>
         <Footer>
-
+            <View style={{flexDirection:'row'}}>
+        <Button style={{marginTop: 5,width:'48%',marginLeft:10,borderRadius:0}} title="ADD TO CART" />
+        {/* <Typography size={20}>|</Typography> */}
+        <Divider orientation="vertical" width={1.1}  color= "#000000" />
+        <Button style={{marginTop: 5,width:'48%',marginRight:10,borderRadius:0}} title="BUY NOW" />
+        </View>
         </Footer>
     </Container>
-);
+    )
+};
 
 const styles = StyleSheet.create({
     inputView: {
@@ -119,6 +202,16 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         marginTop: 30
+    },
+    subtitleStyle2:{
+        marginTop:15,
+        paddingTop:0,
+        lineHeight:18,
+        color:theme.colors.textPrimary,
+        fontFamily:"NunitoLight",
+        fontSize:18,
+        marginLeft:10,
+        textDecorationLine: 'line-through'
     },
     buttonCard: {
 
